98ea417c18e91eb19e48d84f329d853a519423aa
Lean-b <pentestinglean@gmail.com>
2023-12-30 19:07:58 -0300

    Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::env::{self, Args};
+use rand::Rng;
+
+/// Default long 10
+const LONG = 10;
+
+/// Long = Xn = L 
+/// LONG = 10
+/// Number = -n
+/// Letter = -l 
+/// Characters = -c
+
+fn longs (){
+    
+}
+
+fn numbers() {
+    
+}
+
+fn letters() {
+    
+}
+
+fn characters() {
+    
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect(); 
+
+    for argument in args.len(){
+        match argument {
+            
+        }
+    }
 }
